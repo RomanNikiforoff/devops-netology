34acad51a9c4dc9175a25b3edbb1706a1f216331
Roman Nikiforov <108190231+RomanNikiforoff@users.noreply.github.com>
2022-09-30 16:18:00 +0300

    Update producer.py

--- scripts/producer.py
+++ scripts/producer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import pika
 for i in range(1, 10000):
-  credentials = pika.PlainCredentials('nrv', 'Minotavr6268')
+  credentials = pika.PlainCredentials('username', 'userpass')
   #parameters = pika.ConnectionParameters('localhost', 5672, 'test_host', credentials)
   #connection = pika.BlockingConnection(parameters)
-  connection = pika.BlockingConnection(pika.ConnectionParameters('130.193.50.203', 5672, 'test_host', credentials))
+  connection = pika.BlockingConnection(pika.ConnectionParameters('IP', 5672, 'test_host', credentials))
   channel = connection.channel()
   channel.queue_declare(queue='hello')
   channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
