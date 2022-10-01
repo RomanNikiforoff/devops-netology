9b298dd58c88bfd7ac1aebe7292a67e3e4246912
rnikiforov <RNikiforov@synergy.ru>
2022-10-01 21:22:25 +0300

    add files

--- scripts/consumer-original.py
+++ scripts/consumer-original.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
+#!/usr/bin/env python
+# coding=utf-8
 import pika
 
+#connection = pika.BlockingConnection(pika.ConnectionParameters('62.84.118.1', 5672, '/', 'test_user', 'test'))
+#channel = connection.channel()
 credentials = pika.PlainCredentials('test_user', 'test')
 parameters = pika.ConnectionParameters('62.84.118.1', 5672, '/', credentials)
 connection = pika.BlockingConnection(parameters)
-
-channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
-    print("Received %r" % body)
+    print(" [x] Received %r" % body)
 
-channel.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
 
+channel.basic_consume(callback, queue='hello', no_ack=True)
 channel.start_consuming()
-
-print('To exit press CTRL+C')
-connection.close()
--- scripts/producer.py
+++ scripts/producer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import pika
 for i in range(1, 10000):
-  credentials = pika.PlainCredentials('username', 'userpass')
+  credentials = pika.PlainCredentials('test_user', 'test')
   #parameters = pika.ConnectionParameters('localhost', 5672, 'test_host', credentials)
   #connection = pika.BlockingConnection(parameters)
-  connection = pika.BlockingConnection(pika.ConnectionParameters('IP', 5672, 'test_host', credentials))
+  connection = pika.BlockingConnection(pika.ConnectionParameters('62.84.118.1', 5672, '/', credentials))
   channel = connection.channel()
   channel.queue_declare(queue='hello')
   channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
