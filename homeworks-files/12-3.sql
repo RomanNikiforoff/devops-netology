5c48bb7cc5826058ed9ae00784ba496d6a6e55d5
rnikiforov <RNikiforov@synergy.ru>
2022-10-09 19:43:15 +0300

    sql 12-3 added

---
+++ homeworks-files/12-3.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT district from address
+WHERE district like'K%a' and district not like '% %';
+
+SELECT * FROM payment p
+WHERE payment_date BETWEEN '2005-06-15'and '2005-06-18'
+AND amount > 10
+ORDER BY payment_date;
+
+SELECT * FROM rental r 
+order by rental_date DESC  LIMIT 5;
